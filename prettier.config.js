761195c0912b19a687e0e99ff891ed2d1d6ee81d
Nick Machia <nmachia@terakeet.com>
2022-05-16 08:43:31 -0400

    Add linting configuration

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	endOfLine: 'lf',
+	jsxBracketSameLine: false,
+	jsxSingleQuote: true,
+	printWidth: 80,
+	proseWrap: 'never',
+	quoteProps: 'as-needed',
+	semi: true,
+	singleQuote: true,
+	tabWidth: 4,
+	useTabs: true,
+	trailingComma: 'es5',
+	vueIndentScriptAndStyle: true,
+};
