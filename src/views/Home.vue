ca103b19ba198f5dfa393035d0092f7b9e76a596
Nick Machia <nmachia@terakeet.com>
2022-05-17 08:25:39 -0400

    Clean up default Vue App scaffolding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
 	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<h1>Home</h1>
+		<ul>
+			<li><router-link to="/customer-reports">Customer Reports</router-link></li>
+		</ul>
 	</div>
 </template>
 
 <script>
-	// @ is an alias to /src
-	import HelloWorld from '@/components/HelloWorld.vue';
-
 	export default {
 		name: 'Home',
-		components: {
-			HelloWorld,
-		},
 	};
 </script>
