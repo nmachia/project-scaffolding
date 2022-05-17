8ddaaeab75af2b849885dcb079c96e5dd4357297
Nick Machia <nmachia@terakeet.com>
2022-05-17 09:18:20 -0400

    Configure Vue CLI to properly transpile vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
+
 module.exports = {
+	configureWebpack: {
+		plugins: [new VuetifyLoaderPlugin()],
+	},
 	chainWebpack: (config) => {
 		const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
 		types.forEach((type) => addStyleResource(config.module.rule('scss').oneOf(type)));
 	},
-	transpileDependencies: ['vuetify'],
 };
 function addStyleResource(rule) {
 	rule.use('style-resource')
